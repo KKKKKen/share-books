b5ed0f494c24b82b9a57945541d6b255e5365d5f
KKKKKen <tajiken.ho@gmail.com>
2021-06-16 09:52:52 +0900

    delete

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -47,7 +47,7 @@
     </li>
 
     <li class="nav-item">
-      <a class="nav-link" href=""><i class="fas fa-pen mr-1"></i>投稿する</a>
+      <a class="nav-link" href="{{ route('post.create') }}"><i class="fas fa-pen mr-1"></i>投稿する</a>
     </li>
   </ul>
 </div>
