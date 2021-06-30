fe1d34060d0f3bcd1292cf4021de641e25d82c92
KKKKKen <tajiken.ho@gmail.com>
2021-07-01 07:40:05 +0900

    master

--- resources/views/myfavorites.blade.php
+++ resources/views/myfavorites.blade.php
@@ -11,10 +11,9 @@
 
 
     <div class="card mb-3 shadow">
-      <div class="card-body d-flex flex-row">
+      <div class="card-body d-flex justify-content-between">
       
         <div>
-
           <div class="font-weight-bold">
           <img src="{{asset('storage/avatar/'.($post->user->avatar??'user_default.jpg'))}}"
           class="rounded-circle" style="width:40px;height:40px;">
@@ -25,6 +24,65 @@
           {{ $post->created_at->format('Y/m/d  G:i') }} 
           </div>
         </div>
+
+        
+        <!-- お気に入りアイコン↓ -->  
+        <div class="">     
+        @if(Auth::check())
+        @if($post->favorites->count() == 0)
+        <form method="post" action="{{ route('favorite.store', $post) }}">
+            @csrf  
+            <button class="clear-decoration">
+            <i class="fas fa-bookmark fa-2x gray hover"></i>
+          </button class="clear-decoration">
+        </form>
+        @endif
+        <!-- 削除 -->
+        @if($post->favorites->count())
+        <form method="post" action="{{ route('favorite.destroy', $post) }}">
+            @csrf  
+            @method('delete')
+            <button class="clear-decoration">
+              <i class="fas fa-bookmark fa-2x yellow hover"></i>
+            <!-- <i class="fa-solid fa-heart m-0 p-1 {{ $post->favorite == null ? 'bg-brown': '' }}"></i> -->
+          </button>
+        </form>
+        @endif
+        @endif
+        <!-- お気に入りアイコン↑ --> 
+
+
+          <!-- dropdown -->
+          @if($post->user_id == Auth::id())
+          <div class="ml-auto card-text float-end text-end">
+            <div class="dropdown">
+
+              <button class="btn dropdown" type="button" id="dropdownMenuButton" data-bs-toggle="dropdown" aria-expanded="false"> <i class="fas fa-ellipsis-v"></i></button>
+              <div class="dropdown-menu dropdown-menu-right">
+                <a class="dropdown-item" href="{{ route('post.edit', $post) }}">
+                  <i class="fas fa-pen mr-1"></i>記事を更新する
+                </a>
+                <div class="dropdown-divider"></div>
+                <!-- aタグにかいてある↓ -->
+                <!-- data-toggle="modal" data-target="#modal-delete-{{ $post->id }}" -->
+                <a class="dropdown-item text-danger" href="#" id="destroy-post">
+                  <i class="fas fa-trash-alt mr-1"></i>記事を削除する
+                </a>
+<!-- ここにあったformの位置 -->
+              </div>
+              <form method="post" action="{{ route('post.destroy', $post) }}">
+                @csrf
+                @method('delete')
+                <!-- <input type="hidden" id="destroy-post-form" onclick="return alert('OK’)"> -->
+                <!-- <input type="submit" id="destroy-post-form" onclick="return alert('OK’)"> -->
+                <button type="hidden" class="btn" id="destroy-post-form" onclick=" confirm('本当にいいの？');"></button>
+                </form>
+            </div>
+          </div>
+          @endif
+          <!-- dropdown -->
+      </div>
+
       </div>
       <div class="card-body pt-0 pb-2">
         
@@ -53,7 +111,7 @@
                 </div>
 
       </div>
-    </div>
+</div>
  
 @endforeach
 
@@ -62,8 +120,8 @@
 <h4 class="text-center mt-3 opacity rounded" style="background-color:white;">まだお気に入り登録していません</h4>
 @endif
 
+@endsection
 
-
-
-
+@section('script')
+@include('layouts.script')
 @endsection
